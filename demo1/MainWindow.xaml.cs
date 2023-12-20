def72f0a2deb2ffcc23bff0bc0916260e7a57e10
operador <operador@DESKTOP-3I5DUAQ>
2023-12-20 20:19:10 +0100

    Agregar archivos de proyecto.

---
+++ demo1/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace demo1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ demo1/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    public class Photo
+    {
+        public Photo()
+        {
+        }
+        public Photo(string path)
+        {
+            Source = path;
+        }
+       
+
+        public string Source { get; set; }
+
+        public override string ToString() => Source;
+    }
+}
